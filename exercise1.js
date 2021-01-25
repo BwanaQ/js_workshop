85a458bf045854fdf71095bc1e368c0a848aaab4
BwanaQ <thunjawax@gmail.com>
2021-01-25 15:23:03 +0300

    Submit Exercise 1

---
+++ exercise1.js
@@ -0,0 +1,17 @@
+const countriesAPI = 'https://restcountries.eu/rest/v2/all'
+const fetchData = async() => {
+    try {
+        const response = await fetch(countriesAPI)
+        const countries = await response.json()
+        countries.forEach(country => {
+            languages = ""
+            country.languages.forEach(languageItem => {
+                languages += languageItem.name + ", "
+            })
+            console.log("Country: " + country.name + " Capital:  " + country.capital + " Languages: " + languages + " Population: " + country.population + " Area: " + country.area)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+fetchData()
